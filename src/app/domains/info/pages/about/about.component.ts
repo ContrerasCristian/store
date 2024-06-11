f5047ac79bc0579e052a100c75c03c83690e4319
ContrerasCristian <cristiancontreras883@gmail.com>
2024-06-11 11:51:18 -0500

    uso de router link, creacion de page 404

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import {CounterComponent} from '../../../shared/components/counter/counter.component';
-
+import {HighlightDirective} from '../../../shared/directives/highlight.directive';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CounterComponent],
+  imports: [CounterComponent, HighlightDirective, HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
